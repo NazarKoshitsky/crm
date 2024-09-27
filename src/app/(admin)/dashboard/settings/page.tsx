66af81ba5266128aabaa6877d5ba8b602d989a28
Nazar Koshytskyi <77495036+NazarKoshitsky@users.noreply.github.com>
2024-09-27 19:07:49 +0200

    upd

---
+++ src/app/(admin)/dashboard/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export interface SettingsProps {}
+
+export default function Page({}: SettingsProps) {
+  return (
+    <main>
+      <h1 className="text-xl">Settings Page</h1>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-import StatusLabel, { Status } from '@/app/components/status-label';
 import AddCompanyButton from './components/add-company-button';
-import ClientComponent from './components/client-component';
-import ServerComponent from './components/server-component';
-import ServerComponentCopy from './components/server-component-copy';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
       <AddCompanyButton />
-      <ServerComponent />
-      <ClientComponent />
-      <ServerComponentCopy />
-      <ClientComponent />
     </main>
   );
 }
